e2e03f89ab57c97e94b5c21e595d58dc9406d5bd
Beth-Hollinger <hollingere15@gmail.com>
2022-08-29 21:05:21 -0400

    Magic Eight Ball Created

--- server.js
+++ server.js
@@ -26,10 +26,13 @@ app.get("/tip/:total/:tipPercentage", function(request, response){
 const magic = 
 ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"];
 
-app.get("/magic", function(request, response){
-    response.send('Hello stranger!')
+const random = magic[Math.floor(Math.random()* magic.length)]
+
+app.get("/magic/:random", function(request, response){
+    response.send(`Magic ball says ${random}`)
 });
 
 app.listen(3000, function(){
     console.log("What's up Beth?")
 })
+
